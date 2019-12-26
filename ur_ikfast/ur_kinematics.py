6276bb818a18b681882b38f1f2eb591512329260
Cristian Beltran <nanaki07@gmail.com>
2019-12-26 18:23:25 +0900

    Assume quaternions of the form [x,y,z,w]

--- ur_ikfast/ur_kinematics.py
+++ ur_ikfast/ur_kinematics.py
@@ -18,7 +18,14 @@ class URKinematics():
         self.n_joints = self.kinematics.getDOF()
 
     def forward(self, joint_angles, rotation_type='quaternion'):
-
+        """
+            Compute robot's forward kinematics for the specified robot
+            joint_angles: list
+            rotation_type: 'quaternion' or 'matrix'
+            :return: if 'quaternion' then return a list of [x, y, z, w. qx, qy, qz]
+                     if 'matrix' then a list of 12 values the 3x3 rotation matrix and 
+                     the 3 translational values
+        """
         if isinstance(joint_angles, np.ndarray):
             joint_angles = joint_angles.tolist()
         
@@ -31,14 +38,31 @@ class URKinematics():
             return transformations.pose_quaternion_from_matrix(ee_pose)
 
     def inverse(self, ee_pose, all_solutions=False, q_guess=np.zeros(6)):
-        joint_configs = self.kinematics.inverse(ee_pose.reshape(-1).tolist())
+        """ Compute robot's inverse kinematics for the specified robot
+            ee_pose: list of 7 if quaternion [x, y, z, w, qx, qy, qz]
+                     list of 12 if rotation matrix + translational values
+            all_solutions: whether to return all the solutions found or just the best one
+            q_guess:  if just one solution is request, this set of joint values will be use
+                      to find the closest solution to this
+            :return: list of joint angles
+                     list of best joint angles if found
+                     q_guess if no solution is found
+        """
+        pose = None
+        if len(ee_pose) == 7:
+            rot = np.roll(ee_pose[3:], 1)
+            pose = np.concatenate((ee_pose[:3], rot), axis=0)
+        else:
+            pose = ee_pose
+        joint_configs = self.kinematics.inverse(pose.reshape(-1).tolist())
         n_solutions = int(len(joint_configs)/self.n_joints)
         joint_configs = np.asarray(joint_configs).reshape(n_solutions, self.n_joints)
 
         if all_solutions:
             return joint_configs
 
-        return best_ik_sol(joint_configs, q_guess)
+        best_q = best_ik_sol(joint_configs, q_guess)
+        return q_guess if best_q is None else best_q
 
 
 def best_ik_sol(sols, q_guess, weights=np.ones(6)):
@@ -56,7 +80,6 @@ def best_ik_sol(sols, q_guess, weights=np.ones(6)):
         if np.all(test_sol != 9999.):
             valid_sols.append(test_sol)
     if not valid_sols:
-        print("ik failed")
         return None
     best_sol_ind = np.argmin(
         np.sum((weights * (valid_sols - np.array(q_guess)))**2, 1))
