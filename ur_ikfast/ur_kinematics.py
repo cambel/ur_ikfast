bec3389bb7a021f1838d5f336497a230a8c6583b
cambel <cambel@users.noreply.github.com>
2021-04-26 20:52:49 +0900

    Remove dependency on ur_control

--- ur_ikfast/ur_kinematics.py
+++ ur_ikfast/ur_kinematics.py
@@ -1,6 +1,45 @@
+import math
 import numpy as np
 from ur_control import transformations
 
+def quaternion_from_matrix(matrix):
+    """Return quaternion from rotation matrix.
+    >>> R = rotation_matrix(0.123, (1, 2, 3))
+    >>> q = quaternion_from_matrix(R)
+    >>> numpy.allclose(q, [0.0164262, 0.0328524, 0.0492786, 0.9981095])
+    True
+    """
+    q = np.empty((4, ), dtype=np.float64)
+    M = np.array(matrix, dtype=np.float64, copy=False)[:4, :4]
+    t = np.trace(M)
+    if t > M[3, 3]:
+        q[3] = t
+        q[2] = M[1, 0] - M[0, 1]
+        q[1] = M[0, 2] - M[2, 0]
+        q[0] = M[2, 1] - M[1, 2]
+    else:
+        i, j, k = 0, 1, 2
+        if M[1, 1] > M[0, 0]:
+            i, j, k = 1, 2, 0
+        if M[2, 2] > M[i, i]:
+            i, j, k = 2, 0, 1
+        t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+        q[i] = t
+        q[j] = M[i, j] + M[j, i]
+        q[k] = M[k, i] + M[i, k]
+        q[3] = M[k, j] - M[j, k]
+    q *= 0.5 / math.sqrt(t * M[3, 3])
+    return q
+
+def pose_quaternion_from_matrix(matrix):
+    """Return translation + quaternion(x,y,z,w)
+    """
+    if matrix.shape == (3, 4):
+        matrix = np.concatenate((matrix, [[0, 0, 0, 1]]), axis=0)
+
+    pose = matrix[:3, 3]
+    quat = quaternion_from_matrix(matrix)
+    return np.concatenate((pose, quat), axis=0)
 
 class URKinematics():
 
@@ -35,7 +74,7 @@ class URKinematics():
         if rotation_type == 'matrix':
             return ee_pose
         elif rotation_type == 'quaternion':
-            return transformations.pose_quaternion_from_matrix(ee_pose)
+            return pose_quaternion_from_matrix(ee_pose)
 
     def inverse(self, ee_pose, all_solutions=False, q_guess=np.zeros(6)):
         """ Compute robot's inverse kinematics for the specified robot
